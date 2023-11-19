2e1c4b4b9bbab596416491663d440c5acfa9c3ec
kimhyeonju <hyeonju0014@naver.com>
2023-11-19 22:18:11 +0900

    feat notificatoin-oneHour

--- alert.js
+++ alert.js
@@ -5,20 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
             if (permission === 'granted') {
                 // 권한이 허용된 경우 알림 표시
                 showNotification();
-                // const notification = new Notification("캐-안습", {
-                //     body: "안구건조증을 방지하기 위한 서비스가 시작됩니다.",
-                //     icon: "assets/eyeDrop.png" // 알림에 표시될 아이콘 경로
-                // });
+                
             }
         });
     } else if (Notification.permission === 'granted') {
         // 이미 권한이 허용되어 있을 경우 알림 바로 표시
         showNotification();
-        // const notification = new Notification("캐-안습", {
-        //     body: "안구건조증을 방지하기 위한 서비스가 시작됩니다.",
-        //     icon: "assets/eyeDrop.png" // 알림에 표시될 아이콘 경로
-        // });
+    
     }
+    const animeButton = document.querySelector('#animation_toggle');
+    animeButton.addEventListener('click', () => {
+        animeButton.classList.toggle('active');
+        showOneHour(); // 3600000 밀리초 = 1시간    
+    });
+    // setInterval(showOneHour, 3600000); // 3600000 밀리초 = 1시간
 });
 
 function showOneStep() {
@@ -46,22 +46,31 @@ function showOneHour() {
     })
 }
 
-// 알림 클릭 이벤트 핸들러 설정
-// document.addEventListener('click', function(e) {
-//     // 알림을 클릭한 경우만 처리
-//     if (e.target instanceof Notification) {
-//       // 원하는 페이지로 이동
-//       window.location.href = '/anime.html';
-//     }
-// });
 function showNotification() {
     const notification = new Notification("캐-안습", {
         body: "안구건조증을 방지하기 위한 서비스가 시작됩니다.",
         icon: "assets/eyeDrop.png" // 알림에 표시될 아이콘 경로
     });
+}
 
-    // 알림 클릭 이벤트 리스너 추가
-    notification.onclick = function() {
-        window.location.href = 'anime.html'; // 이 부분에 원하는 페이지의 URL을 지정하세요.
-    };
+function showOneHour() {
+    // const notification = new Notification("캐-안습", {
+    //     body: "한시간",
+    //     icon: "assets/eyeDrop.png" // 알림에 표시될 아이콘 경로
+    // });
+    // // 알림 클릭 이벤트 리스너 추가
+    // notification.onclick = function() {
+    //     window.location.href = 'anime.html'; // 이 부분에 원하는 페이지의 URL을 지정하세요.
+    // };
+    if (Notification.permission === 'granted') {
+        const notification = new Notification("캐-안습", {
+            body: "한 시간이 지났습니다. 애니메이션 운동 어때요 ?",
+            icon: "assets/eyeDrop.png" // 알림에 표시될 아이콘 경로
+        });
+
+        // 알림 클릭 시 페이지 이동 (선택적)
+        notification.onclick = function() {
+            window.location.href = 'anime.html'; // 페이지 URL 변경 가능
+        };
+    }
 }
--- home.js
+++ home.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // alert('카메라 접근 권한을 허용하시겠습니까?');
     // alert('사운드 접근 권한을 허용하시겠습니까?');
 
-    const toggleSwitches = document.querySelectorAll('.toggle-switch');
-    toggleSwitches.forEach(switchElement => {
+    const blinkToggle = document.querySelectorAll('#blink_toggle');
+    blinkToggle.forEach(switchElement => {
+        switchElement.addEventListener('click', () => {
+            switchElement.classList.toggle('active');
+            // 토글 상태에 따른 추가적인 동작을 여기에 구현할 수 있습니다.
+        });
+    });
+    const lightToggle = document.querySelectorAll('#light_toggle');
+    lightToggle.forEach(switchElement => {
         switchElement.addEventListener('click', () => {
             switchElement.classList.toggle('active');
             // 토글 상태에 따른 추가적인 동작을 여기에 구현할 수 있습니다.
